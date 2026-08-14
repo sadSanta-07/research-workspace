3d634b3339733d4409457defeef14fa84ba08a26
pechkas <iffcogc34@gmail.com>
2026-08-14 22:32:42 +0530

    feat: add TypeScript definitions for Electron API and chat functionality

---
+++ src/preload/index.d.ts
@@ -0,0 +1,18 @@
+import { ElectronAPI } from '@electron-toolkit/preload'
+import { Workspace } from '../main/db'
+
+declare global {
+  interface Window {
+    electron: ElectronAPI
+    api: {
+      getWorkspaces: () => Promise<Workspace[]>
+      createWorkspace: (name: string) => Promise<Workspace>
+      startChat: (prompt: string, conversationId: string) => void
+      cancelChat: (conversationId: string) => void
+      onChatChunk: (conversationId: string, callback: (chunk: string) => void) => void
+      onChatComplete: (conversationId: string, callback: (fullText: string) => void) => void
+      onChatError: (conversationId: string, callback: (error: string) => void) => void
+      removeChatListeners: (conversationId: string) => void
+    }
+  }
+}
